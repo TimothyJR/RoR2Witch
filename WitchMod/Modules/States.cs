c12c52b54b812eacc014afd801a4e1c1d0e6db39
Timothy Reuter <timothyjreuter@gmail.com>
2021-05-01 14:54:31 -0400

    Rename folders and files for new character.

---
+++ WitchMod/Modules/States.cs
@@ -0,0 +1,24 @@
+﻿using WitchMod.SkillStates;
+using WitchMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace WitchMod.Modules
+{
+	public static class States
+	{
+		internal static List<Type> entityStates = new List<Type>();
+
+		internal static void RegisterStates()
+		{
+			entityStates.Add(typeof(BaseMeleeAttack));
+			entityStates.Add(typeof(SlashCombo));
+
+			entityStates.Add(typeof(Shoot));
+
+			entityStates.Add(typeof(Roll));
+
+			entityStates.Add(typeof(ThrowBomb));
+		}
+	}
+}
